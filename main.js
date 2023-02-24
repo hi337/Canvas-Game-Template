aca5e31aa846300ca54da741480a4bc78fd5c5e1
hi337 <ajamiehrateb@gmail.com>
2023-02-23 18:46:24 -0700

    pause function inside canvas

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ let minuite,
   score = 0;
 let top_score = +window.localStorage.getItem("top_score") || 0;
 let acceleration = 0;
-let pause = true;
+let paused = false;
 
 function endPause() {
   pause = true;
@@ -24,6 +24,7 @@ function startGame() {
   myScore = new text_comp("70px", "Consolas", "black", 200, 40);
   topScore = new text_comp("70px", "Consolas", "black", 330, 40);
   Health = new text_comp("70px", "Consolas", "black", 460, 40);
+  pausedText = new text_comp("100px", "Consolas", "black", 340, 195);
 }
 
 //Where the canvas element is initialized and controlled
@@ -53,121 +54,127 @@ var myGameArea = {
 
 //what happens everytime the frame updates
 function updateGameArea() {
-  if (mainCharacter.crashWith(borderTop)) {
-    mainCharacter.y = 355;
-  } else if (mainCharacter.crashWith(borderBottom)) {
-    mainCharacter.y = 13;
-  } else if (mainCharacter.crashWith(borderRight)) {
-    mainCharacter.x = 13;
-  } else if (mainCharacter.crashWith(borderLeft)) {
-    mainCharacter.x = 655;
-  }
-  myGameArea.clear();
-  borderTop.update();
-  borderBottom.update();
-  borderLeft.update();
-  borderRight.update();
-
-  //detecting r for reset and p for pause
-  if (myGameArea.keys && myGameArea.keys[82]) {
-    location.reload();
-  }
   if (myGameArea.keys && myGameArea.keys[80]) {
-    if (pause) {
+    if (!paused) {
+      paused = true;
+    } else {
+      paused = false;
+    }
+  }
+
+  if (!paused) {
+    if (mainCharacter.crashWith(borderTop)) {
+      mainCharacter.y = 355;
+    } else if (mainCharacter.crashWith(borderBottom)) {
+      mainCharacter.y = 13;
+    } else if (mainCharacter.crashWith(borderRight)) {
+      mainCharacter.x = 13;
+    } else if (mainCharacter.crashWith(borderLeft)) {
+      mainCharacter.x = 655;
+    }
+    myGameArea.clear();
+    borderTop.update();
+    borderBottom.update();
+    borderLeft.update();
+    borderRight.update();
+
+    //detecting r for reset and p for pause
+    if (myGameArea.keys && myGameArea.keys[82]) {
+      location.reload();
+    }
+
+    time_before_next_shot++;
+
+    if (time_before_next_shot >= interval) {
+      choose_shooting_border();
+      if (chosen_border == "top") {
+        borderTop.shoot();
+      } else if (chosen_border == "bottom") {
+        borderBottom.shoot();
+      } else if (chosen_border == "left") {
+        borderLeft.shoot();
+      } else if (chosen_border == "right") {
+        borderRight.shoot();
+      }
+      time_before_next_shot = 0;
+
+      if (interval > 10) {
+        interval -= 0.1;
+      }
+    }
+
+    for (var y = 0; y < bullet_array.length; y++) {
+      if (
+        bullet_array[y].crashWith(borderBottom) ||
+        bullet_array[y].crashWith(borderLeft) ||
+        bullet_array[y].crashWith(borderTop) ||
+        bullet_array[y].crashWith(borderRight)
+      ) {
+        delete bullet_array[y];
+      } else if (bullet_array[y].crashWith(mainCharacter)) {
+        delete bullet_array[y];
+        health -= 1;
+      }
+
+      bullet_array = bullet_array.filter((item) => item !== undefined);
+
+      bullet_array[y].newPos();
+      bullet_array[y].update();
+    }
+
+    // processing the one point per second rule
+    if (minuite < 50) {
+      minuite++;
+    } else {
+      minuite = 0;
+      score++;
+    }
+    //changing top_score for efficient instant adjustment
+    if (score > top_score) {
+      top_score++;
+    }
+    topScore.text = `TOP SCORE: ${top_score}`;
+    topScore.update();
+
+    //changing the text for myScore
+    myScore.text = `SCORE: ${score}`;
+    myScore.update();
+    //update health
+    if (health <= 0) {
+      window.localStorage.setItem("top_score", top_score);
       myGameArea.stop();
-      pause = false;
-      myGameArea.canvas.style.display = "none";
-      document.getElementById("play_screen").style.display = "flex";
     }
-  }
+    Health.text = `HEALTH: ${health}`;
+    Health.update();
 
-  time_before_next_shot++;
-
-  if (time_before_next_shot >= interval) {
-    choose_shooting_border();
-    if (chosen_border == "top") {
-      borderTop.shoot();
-    } else if (chosen_border == "bottom") {
-      borderBottom.shoot();
-    } else if (chosen_border == "left") {
-      borderLeft.shoot();
-    } else if (chosen_border == "right") {
-      borderRight.shoot();
-    }
-    time_before_next_shot = 0;
-
-    if (interval > 10) {
-      interval -= 0.1;
-    }
-  }
-
-  for (var y = 0; y < bullet_array.length; y++) {
-    if (
-      bullet_array[y].crashWith(borderBottom) ||
-      bullet_array[y].crashWith(borderLeft) ||
-      bullet_array[y].crashWith(borderTop) ||
-      bullet_array[y].crashWith(borderRight)
-    ) {
-      delete bullet_array[y];
-    } else if (bullet_array[y].crashWith(mainCharacter)) {
-      delete bullet_array[y];
-      health -= 1;
+    //detecting shift to accelerate
+    if (myGameArea.keys && myGameArea.keys[16]) {
+      acceleration = 3;
+    } else {
+      acceleration = 0;
     }
 
-    bullet_array = bullet_array.filter((item) => item !== undefined);
+    //Moving the character
+    mainCharacter.speedX = 0;
+    mainCharacter.speedY = 0;
 
-    bullet_array[y].newPos();
-    bullet_array[y].update();
-  }
-
-  // processing the one point per second rule
-  if (minuite < 50) {
-    minuite++;
+    if (myGameArea.keys && (myGameArea.keys[37] || myGameArea.keys[65])) {
+      mainCharacter.speedX = -2 - acceleration;
+    }
+    if (myGameArea.keys && (myGameArea.keys[39] || myGameArea.keys[68])) {
+      mainCharacter.speedX = 2 + acceleration;
+    }
+    if (myGameArea.keys && (myGameArea.keys[38] || myGameArea.keys[87])) {
+      mainCharacter.speedY = -2 - acceleration;
+    }
+    if (myGameArea.keys && (myGameArea.keys[40] || myGameArea.keys[83])) {
+      mainCharacter.speedY = 2 + acceleration;
+    }
+    mainCharacter.newPos();
+    mainCharacter.update();
   } else {
-    minuite = 0;
-    score++;
+    myGameArea.clear();
+    pausedText.text = "PAUSED";
+    pausedText.update();
   }
-  //changing top_score for efficient instant adjustment
-  if (score > top_score) {
-    top_score++;
-  }
-  topScore.text = `TOP SCORE: ${top_score}`;
-  topScore.update();
-
-  //changing the text for myScore
-  myScore.text = `SCORE: ${score}`;
-  myScore.update();
-  //update health
-  if (health <= 0) {
-    window.localStorage.setItem("top_score", top_score);
-    myGameArea.stop();
-  }
-  Health.text = `HEALTH: ${health}`;
-  Health.update();
-
-  //detecting shift to accelerate
-  if (myGameArea.keys && myGameArea.keys[16]) {
-    acceleration = 3;
-  } else {
-    acceleration = 0;
-  }
-
-  //Moving the character
-  mainCharacter.speedX = 0;
-  mainCharacter.speedY = 0;
-
-  if (myGameArea.keys && (myGameArea.keys[37] || myGameArea.keys[65])) {
-    mainCharacter.speedX = -2 - acceleration;
-  }
-  if (myGameArea.keys && (myGameArea.keys[39] || myGameArea.keys[68])) {
-    mainCharacter.speedX = 2 + acceleration;
-  }
-  if (myGameArea.keys && (myGameArea.keys[38] || myGameArea.keys[87])) {
-    mainCharacter.speedY = -2 - acceleration;
-  }
-  if (myGameArea.keys && (myGameArea.keys[40] || myGameArea.keys[83])) {
-    mainCharacter.speedY = 2 + acceleration;
-  }
-  mainCharacter.newPos();
-  mainCharacter.update();
 }
